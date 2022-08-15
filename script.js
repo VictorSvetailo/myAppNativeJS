e25b5883271605943772c72ff66cbb7f03f96e74
VictorSvetailo <victorioeve@gmail.com>
2022-08-15 12:40:17 +0300

    Create trim

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 
 
 let numberOfFilms;
-let favoriteGenre;
 
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    while (numberOfFilms === '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
-start()
+
+// start()
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -23,8 +23,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+            b = prompt('На сколько оцените его?', '').trim();
 
         if (a != null && b != null && a !== '' && b !== "" && a.length < 50) {
             personalMovieDB.movies[a] = b;
@@ -35,7 +35,8 @@ function rememberMyFilms() {
         }
     }
 }
-// rememberMyFilms();
+
+rememberMyFilms();
 
 function detectPersonalLevel() {
     personalMovieDB.count === 0 || '' ? alert("Произошла ошибка") :
@@ -44,6 +45,7 @@ function detectPersonalLevel() {
                 personalMovieDB.count > 30 ? alert("Вы киноман") : alert("Произошла ошибка")
 
 }
+
 // detectPersonalLevel()
 
 function showMyDB(hidden) {
@@ -51,28 +53,13 @@ function showMyDB(hidden) {
         console.log(personalMovieDB);
     }
 }
-showMyDB(personalMovieDB.privat)
+
+// showMyDB(personalMovieDB.privat)
 
 function writeYourGenres(g) {
     for (let i = 1; i <= 3; i++) {
         g[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
     }
 }
-writeYourGenres(personalMovieDB.genres)
 
-
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
-
-// Код возьмите из предыдущего домашнего задания
+// writeYourGenres(personalMovieDB.genres)
